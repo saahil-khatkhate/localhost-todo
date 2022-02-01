a67628a31288762cfc55575f413c2f279367d517
saahil-khatkhate <saahil.khatkhate@gmail.com>
2022-01-31 23:33:36 -0500

    remove router

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './global.css';
+
+const App: React.FC = () => {
+  return (
+    <h1>To-Do</h1>
+  );
+};
+
+export default App;
--- src/pages/error.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Error: React.FC<{ status: number }> = ({ status }) => {
-    return (
-      <h1>{ status == 404 ? "page not found" : "" }</h1>
-    );
-};
-
-export default Error;
